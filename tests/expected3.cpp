53edf947ed709b1a028d8647d46671114fb1291c
ebucher <ebucher@blue.cs.sonoma.edu>
2017-05-03 19:06:11 -0700

    Add script and samples for testing

---
+++ tests/expected3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int tester(int one, int two){
+	cout << one << " " << two << endl;
+	cout << "Those are the numbers" << endl;
+}
+
+
+int main(){
+	tester(5, 6);
+	return 0;
+}
