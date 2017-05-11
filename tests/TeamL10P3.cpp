c6a775d74e5beb95750b6acec6766ea7ef4a75b6
ebucher <ebucher@blue.cs.sonoma.edu>
2017-05-10 18:17:03 -0700

    Fixed test 10

---
+++ tests/TeamL10P3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "Object.h"
+
+using namespace std;
+
+int main(  ) {
+Object _RetVal;
+if(numberp(Object(4) )){
+cout << Object("is_a_number ");
+
+} else {
+cout << Object("is_not_a_number ");
+}
+cout << endl;
+if(stringp(Object(4) )){
+cout << Object("is_a_string ");
+
+} else {
+}
+cout << endl;
+return 0;
+}
--- tests/expected10.cpp
+++ tests/expected10.cpp
@@ -5,13 +5,15 @@ using namespace std;
 int main(){
 	Object _RetVal;
 	if(numberp(Object(4))){
-		cout << "is_a_number";
+		cout << Object("is_a_number");
 	} else {
-		cout << "is_not_a_number";
+		cout << Object("is_not_a_number");
 	}
 	cout << endl;
 	if (stringp(Object(4))){
-		cout << "is_a_string";
+		cout << Object("is_a_string");
+	} else {
+
 	}
 	cout << endl;
 	return 0;
